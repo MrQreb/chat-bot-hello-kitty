af313436a872a04cb6fa37da61aa77a16a6b0e0f
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2025-03-05 14:17:37 -0600

    shadcn installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -96,6 +97,9 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <div>
+      <Button>Click me</Button>
+    </div>
     </div>
   );
 }
